10c1b37fe8f92b1a997712b8cf12f6c8682d7693
Aurora-kite <3154174134@qq.com>
2022-08-26 18:04:24 +0800

    V0.5 增加登录功能

--- myauth/views.py
+++ myauth/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-def 主页(请求):
-    return render(请求, 'myauth/home.html')
+def 主页(request):
+    return render(request, 'myauth/home.html')
 
 
-def 登录(请求):
-    return render(请求, 'myauth/login.html')
+def 登录(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['用户名'], password=request.POST['密码'])
+        if user is None:
+            return render(request, 'myauth/login.html', {'错误': '用户名或密码错误'})
+        else:
+            login(request, user)
+            return redirect('myauth:主页')
+    else:
+        return render(request, 'myauth/login.html')
